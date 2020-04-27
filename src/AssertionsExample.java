0e04e2e135015865c9afc8a3d29b333b504ea36e
Юлия <yanju@tut.by>
2020-04-27 23:20:25 +0300

    add selenium an TC for AssertionsExample

---
+++ src/AssertionsExample.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AssertionsExample {
+    WebDriver driver;
+
+    @BeforeClass
+    void setup() {
+        //Chrome browser
+        System.setProperty("webdriver.chrome.driver", "src/Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @Test
+    void logoTest() {
+        WebElement logo = driver.findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+        Assert.assertTrue(logo.isDisplayed(), "Logo not displayed");
+    }
+
+    @Test
+    void homePageTitleTest() {
+        Assert.assertEquals("OrangeHRM", driver.getTitle(), "Title not matched");
+    }
+
+    @AfterClass
+    void tearDown() {
+        driver.quit();
+    }
+
+}
