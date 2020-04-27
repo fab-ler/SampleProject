9626b395f31e51388dd9c449a2deaa123f67831a
Юлия <yanju@tut.by>
2020-04-27 21:17:35 +0300

    disable / enable annotation example added

--- src/PriorityExample.java
+++ src/PriorityExample.java
@@ -12,12 +12,13 @@ public class PriorityExample {
         System.out.println("this is testTWO");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2, enabled = true)
     void TestTree() {
         System.out.println("this is testTREE");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 3, enabled = false)
+        // enable = false - ignore running this test, without priority tests will run in an alphabetic order of methods name
     void TestFour() {
         System.out.println("this is testFOUR");
     }
